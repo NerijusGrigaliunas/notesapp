9f47fafe1fef5836ccce857f64bf9a2961acd644
Nerijus Grigaliunas <nerijus.grigaliunas@hotmail.com>
2022-03-20 09:21:24 +0200

    get values notes and categorys
    get values notes and categorys

--- main/notes/views.py
+++ main/notes/views.py
@@ -1,11 +1,16 @@
 
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Create your views here.
+from .models import Note
 
 def notes(request):
-    return render(request, 'notes.html')
+    notes = Note.objects.all()
+    context = { 'notes': notes}
+    return render(request, 'notes.html', context)
 
-def note(request , pk):
-    return HttpResponse('note.html')
+def note(request, pk):
+    note = Note.objects.get(id=pk)
+    category = note.category.all()
+    return render(request, 'note.html', {'note': note, 'category': category})
